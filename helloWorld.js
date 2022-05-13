ffa574eac26b791c0b85897f38beb2586ad32687
Jason <jason.morta23@gmail.com>
2022-05-13 15:16:59 +0200

    Updated helloWorld to get and output user input values.

--- helloWorld.js
+++ helloWorld.js
@@ -1,1 +1,11 @@
-console.log("Git is Awesome!")
+//outputs user input
+
+let userAge = prompt("Enter you age:");
+
+if (userAge > 29) {
+   console.log(`"${userAge}"  ¯\\_(ツ)_/¯ not too old.`);
+   alert(`"${userAge}"  ¯\\_(ツ)_/¯ not too old.`);
+} else {
+   console.log(`"${userAge}"  (❁´◡\`❁) Enjoy your youth.`);
+   alert(`"${userAge}"  (❁´◡\`❁) Enjoy your youth.`);
+};
